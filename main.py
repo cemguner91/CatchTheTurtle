81cba6aae269dfc993ae156ad1d0ca8876bcd69f
Cem Güner <cemguner91@icloud.com>
2024-03-17 02:38:15 +0300

    turtle moving

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import turtle
-import random
+from random import randint
+import time
 FONT = ("Arial", 20, "normal")
+score = 0
+zaman = 30
+
+''''
+for i in range(30):
+    zaman = zaman - 1
+
+def clicking():
+    pass
+'''
 
 #screen
 screen = turtle.Screen()
@@ -15,19 +26,27 @@ txt = turtle.Turtle()
 txt.penup()
 txt.hideturtle()
 txt.setposition(0, y)
-txt.write("Kalan Zaman:", align="center",  font=FONT)
+txt.write(f"Kalan Zaman: {zaman}", align="center",  font=FONT)
 
 txt2 = turtle.Turtle()
 txt2.penup()
 txt2.hideturtle()
 txt2.setposition(0, y-25)
-txt2.write("Score:", align="center", font=FONT)
+txt2.write(f"Score: {score}", align="center", font=FONT)
 
 
 #turtle icon
 turtle_icon = turtle.Turtle()
 turtle_icon.shape("turtle")
-turtle_icon.shapesize(3)
+turtle_icon.shapesize(2)
 turtle_icon.color("green")
+
+for i in range(5):
+    turtle_icon.penup()
+    turtle_icon.hideturtle()
+    turtle_icon.goto(randint(0,300), randint(0,300))
+    turtle_icon.showturtle()
+    time.sleep(0.80)
+
 
 turtle.mainloop()
