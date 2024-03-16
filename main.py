2bd4f59fe4fe89bf67c810b2ef579387cd3d71d7
Cem Güner <cemguner91@icloud.com>
2024-03-16 23:34:27 +0300

    turtle icon revision 1

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ screen = turtle.Screen()
 screen.bgcolor("light blue")
 screen.title("Catch The Turtle")
 
-turtle_instance = turtle.shape("turtle")
+turtle_icon = turtle.Turtle()
+turtle_icon.shape("turtle")
+turtle_icon.shapesize(3)
+turtle_icon.color("green")
 
 turtle.mainloop()
