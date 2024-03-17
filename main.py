7d779416586fca5431eeccacd4139694ab53779c
Cem Güner <cemguner91@icloud.com>
2024-03-17 16:32:42 +0300

    new main and classes added

--- main.py
+++ main.py
@@ -1,52 +1,57 @@
 import turtle
-from random import randint
 import time
-FONT = ("Arial", 20, "normal")
-score = 0
-zaman = 30
+from random import randint
 
-''''
-for i in range(30):
-    zaman = zaman - 1
-
-def clicking():
-    pass
-'''
-
-#screen
 screen = turtle.Screen()
 screen.bgcolor("light blue")
 screen.title("Catch The Turtle")
+screen.listen()
 
-#Text
-top_heigh = screen.window_height()/2
-y = top_heigh - top_heigh/10
+FONT = ("Arial", 20, "normal")
+score = 0
+countdown = 10
 
-txt = turtle.Turtle()
-txt.penup()
-txt.hideturtle()
-txt.setposition(0, y)
-txt.write(f"Kalan Zaman: {zaman}", align="center",  font=FONT)
+class Genel():
 
-txt2 = turtle.Turtle()
-txt2.penup()
-txt2.hideturtle()
-txt2.setposition(0, y-25)
-txt2.write(f"Score: {score}", align="center", font=FONT)
+    def __init__(self):
+        self.turtle_icon = turtle.Turtle()
+        self.turtle_icon.shape("turtle")
+        self.turtle_icon.pensize(2)
+        self.turtle_icon.color("green")
 
+        top_heigh = screen.window_height() / 2
+        y = top_heigh - top_heigh / 10
 
-#turtle icon
-turtle_icon = turtle.Turtle()
-turtle_icon.shape("turtle")
-turtle_icon.shapesize(2)
-turtle_icon.color("green")
+        self.txt = turtle.Turtle()
+        self.txt.penup()
+        self.txt.hideturtle()
+        self.txt.setposition(0, y)
+        self.txt.write(f"Kalan Zaman: {countdown}", align="center", font=FONT)
 
-for i in range(5):
-    turtle_icon.penup()
-    turtle_icon.hideturtle()
-    turtle_icon.goto(randint(0,300), randint(0,300))
-    turtle_icon.showturtle()
-    time.sleep(0.80)
+        self.txt2 = turtle.Turtle()
+        self.txt2.penup()
+        self.txt2.hideturtle()
+        self.txt2.setposition(0, y - 25)
+        self.txt2.write(f"Score: {score}", align="center", font=FONT)
 
+        for i in range(countdown, -1, -1):
 
+            def clicking():
+                self.txt2.clear()
+                self.txt2.write(f"Score: {score + 1}", align="center", font=FONT)
+
+            self.turtle_icon.penup()
+            self.turtle_icon.hideturtle()
+            self.turtle_icon.goto(randint(0, 300), randint(0, 300))
+            self.turtle_icon.showturtle()
+            self.turtle_icon.onclick(clicking)
+            self.txt.clear()
+            self.txt.write(f"Kalan Zaman: {i}", align="center", font=FONT)
+            time.sleep(0.80)
+
+            if i == 0:
+                self.txt.clear()
+                self.txt.write("Game Over", align="center", font=FONT)
+
+Genel()
 turtle.mainloop()
